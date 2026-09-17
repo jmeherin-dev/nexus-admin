3ed683b271616093b3da81525e0627e5dbeb3efa
Meherin Jahan <jmeherin7@gmail.com>
2026-09-17 10:52:29 +0600

    fix email transporter

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,9 @@ import nodemailer from 'nodemailer';
 
 // Nodemailer Transporter Config
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
